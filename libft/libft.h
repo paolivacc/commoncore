41dc82c573b6983bb0bdc5e1d8caf2770367870c
Susana Paoli Vaccaro Nunez <svaccaro@c3r4s1.42malaga.com>
2023-09-18 16:28:07 +0200

    Add libft.h

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 16:20:47 by svaccaro          #+#    #+#             */
+/*   Updated: 2023/09/18 16:25:39 by svaccaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+
+#endif /*LIBFT_H*/
+
+
+
