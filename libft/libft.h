ea9f3dfaaef321f6bf0b688b0fe2e08c1816cdb0
Susana Paoli Vaccaro Nunez <svaccaro@c3r4s1.42malaga.com>
2023-09-18 17:47:05 +0200

    Add function ft_strlen.c to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,18 +6,23 @@
 /*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:20:47 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/09/18 16:25:39 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/09/18 17:36:32 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <string.h>
+
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
+size_t ft_strlen(const char *s);
+void *ft_memset(void *b, int c, size_t len);
+
 
 #endif /*LIBFT_H*/
 
