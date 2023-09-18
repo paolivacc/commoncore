8ec842499a82a9d7a6f45cb25dbca1268ed33d18
Susana Paoli Vaccaro Nunez <svaccaro@c3r4s1.42malaga.com>
2023-09-18 16:11:29 +0200

    Add main.c to testing functions

---
+++ libft/main.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 15:53:05 by svaccaro          #+#    #+#             */
+/*   Updated: 2023/09/18 16:10:23 by svaccaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <libft.h>
+
+int main (void)
+{
+	printf("isalpha: %i", isalpha('a'));
+	printf("isalpha: %i", isalpha('A'));
+	printf("isalpha: %i", isalpha('9'));
+	printf("isalpha: %i", isalpha('-'));
+	printf("ft_isalpha: %i");
+	printf("isdigit");
+	printf("ft_isdigit");
+	printf("isalnum");
+	printf("ft_isalnum");
+	printf("isascii");
+	printf("ft_isascii");
+	printf("isprint");
+	printf("ft_isprint");
+	printf("strlen");
+	printf("ft_strlen");
+	printf("memset");
+	printf("ft_memset");
+	printf("bzero");
+	printf("ft_bzero");
+	printf("memcpy");
+	printf("ft_memcpy");
+	printf("memmove");
+	printf("ft_memmove");
+	printf("strlcpy");
+	printf("ft_strlcpy");
+	printf("strlcat");
+	printf("ft_strlcat");
+	printf("toupper");
+	printf("ft_toupper");
+	printf("tolower");
+	printf("ft_tolower");
+	printf("strchr");
+	printf("ft_strchr");
+	printf("strrchr");
+	printf("ft_strrchr");
+	printf("strncmp");
+	printf("ft_strncmp");
+	printf("memchr");
+	printf("ft_memchr");
+	printf("memcmp");
+	printf("ft_memcmp");
+	printf("strnstr");
+	printf("ft_strnstr");
+	printf("atoi");
+	printf("ft_atoi");
+	printf("calloc");
+	printf("ft_calloc");
+	printf("strdup");
+	printf("ft_strdup");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
+	printf("");
+	printf("ft_");
