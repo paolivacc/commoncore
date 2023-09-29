baf13725e2de95aa5bcedadf27027353a5827590
Susana Paoli Vaccaro Nunez <svaccaro@c3r9s1.42malaga.com>
2023-09-29 13:22:08 +0200

    Modify file

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 00:53:38 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/09/29 13:17:13 by svaccaro         ###   ########.fr       */
+/*   Updated: 2023/09/29 13:21:33 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[dstlen + i] = '\0';
 	return (dstlen + ft_strlen(src));
 }
-
-/*int main ()
-{
-	char dst[5] = "Hola";
-	char src[20] = ", adios";
-	int len = strlcat(dst, src, 6);
-	
-	char ft_dst[5] = "Hola";
-	char ft_src[20] = ", adios";
-	int ft_len = ft_strlcat(ft_dst, ft_src, 6);
-	
-	printf("La nueva cadena es: %s\nLa longitud es: %i\n", dst, len);
-	printf("La nueva cadena_ft es: %s\nLa longitud_ft es: %i\n", ft_dst, ft_len);
-	return (0);
-}*/
