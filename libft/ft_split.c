982d9656f570b21cbf94c74114942222676e3a04
Susana Paoli Vaccaro Nunez <svaccaro@c3r7s1.42malaga.com>
2023-10-20 18:45:04 +0200

    Start again with split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,85 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svaccaro <svaccaro@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svaccaro <svaccaro@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 13:24:34 by svaccaro          #+#    #+#             */
-/*   Updated: 2023/10/19 16:02:57 by svaccaro         ###   ########.fr       */
+/*   Created: 2023/10/20 13:58:30 by svaccaro          #+#    #+#             */
+/*   Updated: 2023/10/20 18:42:44 by svaccaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_wordcounter(const char *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	size_t	cnt;
-	size_t	i;
-
-	i = 0;
-	cnt = 0;
-	while (*(s + i))
-	{
-		if ((*(s + i++) != c) && (*(s + i) == c || *(s + i) == '\0'))
-			cnt++;
-	}
-	return (cnt);
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char	**ssplit;
-	char	*substr;
+	char	**split;
 	size_t	wcnt;
-	size_t	wlen;
-	size_t	i;
 
-	wcnt = ft_wordcounter(s, c);
-	i = 0;
-	while (s[i] == c)
-		++i;
-	substr = ft_substr(s, i, ft_strlen(s)-i);
-	ssplit = (char **)malloc((wcnt + 1) * sizeof(char *));
-	if (!ssplit)
-		return (free(substr), NULL);
-	i = 0;
-	while (i < wcnt)
-	{
-		wlen = 0;
-		while (substr[wlen] != c && substr[wlen] != '\0')
-			wlen++;
-		if (wlen > 0)
-		{
-			ssplit[i] = malloc((wlen + 1) * sizeof(char));
-			if (!ssplit[i])
-			{
-				while (--i >= 0)
-					free(ssplit[i]);
-				free(ssplit);
-				return (NULL);
-			}
-			ft_strlcpy(ssplit[i], substr, wlen + 1);
-			i++;
-		}
-		substr = ft_substr(substr, wlen + 1, ft_strlen(substr) - wlen - 1);
-	}
-	free(substr);
-	ssplit[i] = NULL;
-	return (ssplit);
+	return(ssplit);
 }
-
-/*int main (void)
-{
-	int i = 0;
-	char **tabstr;
-	if (!(tabstr = ft_split("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse", ' ')))
-        printf("NULL");
-    else
-    {
-        while (tabstr[i] != NULL)
-        {
-        	printf("%s\n", tabstr[i]);
-            i++;
-        }
-    }
-}
-*/
